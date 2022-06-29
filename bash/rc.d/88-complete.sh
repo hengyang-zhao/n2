dc0cb07c4dd8480469f05e10e0d5b4f4c28a0341
Hengyang Zhao <hzhao877502@gmail.com>
2022-06-29 15:58:57 -0700

    Complete n2 command

--- bash/rc.d/88-complete.sh
+++ bash/rc.d/88-complete.sh
@@ -6,37 +6,11 @@ __n2_complete()
 
     case "${#COMP_WORDS[@]}" in
         2)
-            opts="banner clean info reinstall update"
+            opts="update status help"
             COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
             ;;
-        3)
-            subcmd="${COMP_WORDS[COMP_CWORD-1]}"
-            case "$subcmd" in
-                banner)
-                    opts="on off"
-                    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-                    ;;
-                reinstall)
-                    opts="--overwrite-all --no-backup --force-legacy"
-                    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-                    ;;
-                *)
-                    COMPREPLY=()
-                    ;;
-            esac
-            ;;
         *)
-            subcmd="${COMP_WORDS[1]}"
-            case "$subcmd" in
-                reinstall)
-                    opts="--overwrite-all --no-backup --force-legacy"
-                    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-                    ;;
-                *)
-                    COMPREPLY=()
-                    ;;
-            esac
-            ;;
+            COMPREPLY=()
     esac
 }
 complete -F __n2_complete n2
@@ -55,19 +29,5 @@ __n2_list_hook_labels()
     )
 }
 
-__n2_infinite_bash_complete()
-{
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-
-    case "${#COMP_WORDS[@]}" in
-        2)
-            local opts="$(__n2_list_hook_labels)"
-            COMPREPLY=( $(compgen -W "${opts[*]}" -- "${cur}") )
-            ;;
-        *)
-            COMPREPLY=()
-    esac
-}
-complete -F __n2_infinite_bash_complete __n2_infinite_bash
 complete -F __n2_infinite_bash_complete lab
 complete -F __n2_infinite_bash_complete bashtrap
